2d01f288620bdfdda53da22067f31e87987a1c08
Mokxrock <81343701+Mokxrock@users.noreply.github.com>
2022-05-08 21:42:14 +1000

    Update SIT210_Task7.3D_RPiPWM.py

--- SIT210_Task7.3D_RPiPWM.py
+++ SIT210_Task7.3D_RPiPWM.py
@@ -4,56 +4,47 @@ GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BOARD)
 
-TRIG = 16 # GPIO 23
-ECHO = 18 # GPIO 24
-LED = 12 # GPIO 18
-#i=0
+TRIG = 16 # Physical Pin 16 to GPIO 23 
+ECHO = 18 # Physical Pin 18 to GPIO 24
+LED = 12  # Physical Pin 13 to GPIO 18
 
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
-GPIO.setup(LED,GPIO.OUT)
+GPIO.setup(TRIG,GPIO.OUT) # Set as Output
+GPIO.setup(ECHO,GPIO.IN) # Set as Input
+GPIO.setup(LED,GPIO.OUT) # Set as Output
+ 
+GPIO.output(TRIG, False) # Turn Of Sensor 
 
-GPIO.output(TRIG, False)
-
-p = GPIO.PWM(LED, 50)
-p.start(0);
+p = GPIO.PWM(LED, 100) #LED as PWM output, with 100Hz frequency
+p.start(0); # Starting from 0
 
 try:
     while True:
-        GPIO.output(TRIG, True)
+        GPIO.output(TRIG, True) # Turn the Sensor On and Off between 0.00001 second 
         time.sleep(0.00001)
         GPIO.output(TRIG, False)
         
-        while GPIO.input(ECHO)==0:
+        #set the beggining of the time 
+        while GPIO.input(ECHO)==0: 
             pulse_start = time.time()
-            
+        #set the Ending of the time 
         while GPIO.input(ECHO)==1:
             pulse_end = time.time()
 
         pulse_duration = pulse_end - pulse_start
         
-        distance = round(((pulse_duration * 34000) / 2 ), 2)
+        distance = round(((pulse_duration * 34000) / 2 ), 2) # Calculation of distance by using the echo and sound 
 
-        #distance = round(distance+1.15, 2)
-        print ("distance:",distance,"cm")
+        print ("distance:",distance,"cm") # Print Distance
 
-        if distance<=50:
-            # GPIO.output(LED, GPIO.HIGH)
-            # time.sleep(1)
-            # GPIO.output(LED, GPIO.LOW)
-            # time.sleep(1)
-            for x in range(50,101,5):
-                p.ChangeDutyCycle(x)
-                time.sleep(0.1)
+        if distance<=50:  # Less then or Equal to 50cm
+            for x in range(50,101,10): # Starting from 50 Hertz and End with 100 HErtz and add 10 hertz to each loop 
+                p.ChangeDutyCycle(x) # Change LED brightness
+                time.sleep(0.1) # Stay for  0.1 Second
                 
-        elif distance>50:
-            # GPIO.output(LED, GPIO.HIGH)
-            # time.sleep(5)
-            # GPIO.output(LED, GPIO.LOW)
-            # time.sleep(5)
-            for x in range(11,-1,-5):
-                p.ChangeDutyCycle(x-0)
-                time.sleep(0.1)
+        elif distance>50: # More then 50cm
+            for x in range(50,-1,-10): # Starting from 50 hertz it will decrease by 10 at a time until then 0  
+                p.ChangeDutyCycle(x) # Change LED brightness
+                time.sleep(0.1) # Stay for  0.1 Second
 
 
 except KeyboardInterrupt:
